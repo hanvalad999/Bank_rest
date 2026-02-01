682ecde8b6d199d7d37b27495fdcd9865629901c
hanvalad999 <hanvaladbabaev@yandex.ru>
2026-02-01 19:51:28 +0300

    Revert "Add entities"

--- src/main/java/com/example/bankcards/entity/Card.java
+++ src/main/java/com/example/bankcards/entity/Card.java
@@ -1,44 +1,4 @@
 package com.example.bankcards.entity;
 
-import com.example.bankcards.util.AesCardNumberConverter;
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-@Entity
-@Table(name = "cards")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Card {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @Convert(converter = AesCardNumberConverter.class)
-    @Column(name = "card_number_encrypted", nullable = false, columnDefinition = "text")
-    private String cardNumber;
-
-    @Column(nullable = false, precision = 19, scale = 2)
-    private BigDecimal balance;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 20)
-    private CardStatus status;
-
-    @Column(name = "expiration_date")
-    private LocalDate expirationDate;
-
-    @Version
-    @Column(nullable = false)
-    private Long version;
 }
