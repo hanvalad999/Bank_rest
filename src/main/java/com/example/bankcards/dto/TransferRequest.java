de5db8fee601a143cac750b0f1acb2864482e27d
Hanvalad <hanvaladbabaev@yandex.ru>
2026-02-03 00:44:39 +0300

    feat: add DTO models

---
+++ src/main/java/com/example/bankcards/dto/TransferRequest.java
@@ -0,0 +1,12 @@
+package com.example.bankcards.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record TransferRequest(
+        @NotNull Long fromCardId,
+        @NotNull Long toCardId,
+        @NotNull @Positive BigDecimal amount
+    ) {}
