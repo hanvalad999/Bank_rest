3428ef13fe35672783127c7cc4588d4c73260061
Hanvalad <hanvaladbabaev@yandex.ru>
2026-02-03 00:58:42 +0300

    feat: add repositories and update entities

---
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -0,0 +1,26 @@
+package com.example.bankcards.repository;
+
+import com.example.bankcards.entity.Card;
+import com.example.bankcards.entity.CardStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import jakarta.persistence.LockModeType;
+
+import java.util.Optional;
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+    Page<Card> findAllByUserUsername(String username, Pageable pageable);
+
+    Page<Card> findAllByUserUsernameAndStatus(String username, CardStatus status, Pageable pageable);
+
+    Page<Card> findAllByStatus(CardStatus status, Pageable pageable);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select c from Card c where c.id = :id")
+    Optional<Card> findByIdForUpdate(@Param("id") Long id);
+}
